604242312efda19eb55d3f3941c6824e3674e25a
Taran Lynn (lambda-11235) <taranlynn0@gmail.com>
2016-03-05 18:38:49 -0800

    Fixed Font to use UTF-8 encodings instead of plain ASCII.

--- tsc/src/video/font.cpp
+++ tsc/src/video/font.cpp
@@ -73,11 +73,14 @@ void cFont_Manager::Queue_Text(const sf::Text& text)
  */
 void cFont_Manager::Prepare_SFML_Text(sf::Text& text, const std::string& str, float x, float y, int fontsize /* = FONTSIZE_NORMAL */, const Color color /* = black */, bool ignore_camera /* = false */)
 {
+    std::basic_string<sf::Uint32> tmp;
+    sf::Utf8::toUtf32(str.begin(), str.end(), std::back_inserter(tmp));
+    sf::String utf8Str = tmp;
 
     text.setFont(m_font_normal);
     text.setColor(color.Get_SFML_Color());
     text.setCharacterSize(fontsize);
-    text.setString(str);
+    text.setString(utf8Str);
 
     if (ignore_camera) {
         // SFML thinks 0|0 is left top of the window
