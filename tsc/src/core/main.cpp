6594917ee98e0f043781233c7333d59608e9480b
Quintus <quintus@quintilianus.eu>
2016-03-20 14:40:50 +0100

    Port display size event and some converts in main.cpp to CEGUI 0.8.

--- tsc/src/core/main.cpp
+++ tsc/src/core/main.cpp
@@ -191,9 +191,9 @@ int main(int argc, char** argv)
             Game_Action_Data_Middle.add("load_menu", int_to_string(MENU_MAIN));
         }
 
-        Game_Action_Data_Start.add("screen_fadeout", CEGUI::PropertyHelper::intToString(EFFECT_OUT_BLACK));
+        Game_Action_Data_Start.add("screen_fadeout", int_to_string(EFFECT_OUT_BLACK));
         Game_Action_Data_Start.add("screen_fadeout_speed", "3");
-        Game_Action_Data_End.add("screen_fadein", CEGUI::PropertyHelper::intToString(EFFECT_IN_BLACK));
+        Game_Action_Data_End.add("screen_fadein", int_to_string(EFFECT_IN_BLACK));
         Game_Action_Data_End.add("screen_fadein_speed", "3");
 
         // game loop
@@ -468,7 +468,7 @@ bool Handle_Input_Global(const sf::Event& ev)
         return 0;
     }
     case sf::Event::Resized: {
-        pGuiSystem->notifyDisplaySizeChanged(CEGUI::Size(static_cast<float>(ev.size.width), static_cast<float>(ev.size.height)));
+        CEGUI::System::getSingleton().notifyDisplaySizeChanged(CEGUI::Sizef(static_cast<float>(ev.size.width), static_cast<float>(ev.size.height)));
         break;
     }
     case sf::Event::TextEntered: {
