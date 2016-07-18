d513fee4d97773b58b2bf54df508b52280f572e7
Quintus <quintus@quintilianus.eu>
2016-07-18 20:33:16 +0200

    Adapt path to new editor config panel.

--- tsc/src/objects/path.hpp
+++ tsc/src/objects/path.hpp
@@ -194,6 +194,8 @@ namespace TSC {
 #ifdef ENABLE_EDITOR
         // level editor activation
         virtual void Editor_Activate(void);
+        // level editor deactivation
+        virtual void Editor_Deactivate(void);
         // editor state update
         virtual void Editor_State_Update(void);
         // editor identifier text changed event
@@ -218,6 +220,12 @@ namespace TSC {
         bool Editor_Pos_Y2_Text_Changed(const CEGUI::EventArgs& event);
         // set linked path states to move again from start of the current segment
         void Editor_Segment_Pos_Changed(void);
+
+        CEGUI::Combobox* mp_segment_box;
+        CEGUI::Editbox* mp_x1_box;
+        CEGUI::Editbox* mp_x2_box;
+        CEGUI::Editbox* mp_y1_box;
+        CEGUI::Editbox* mp_y2_box;
 #endif
 
         // string identifier (so objects can link to us)
