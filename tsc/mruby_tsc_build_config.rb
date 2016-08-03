89591c21c81435069f63ce2a78014dcf3461e363
Quintus <quintus@quintilianus.eu>
2016-08-03 11:38:57 +0200

    Enable mruby's UTF-8 string facilities.

--- tsc/mruby_tsc_build_config.rb
+++ tsc/mruby_tsc_build_config.rb
@@ -33,6 +33,13 @@ end
 MRuby::Build.new do |conf|
   toolchain :gcc
   enable_debug
+
+  # Define MRB_UTF8_STRING to make mruby's String class able
+  # to handle non-ascii characters properly.
+  conf.cc do |cc|
+    cc.flags += ["-DMRB_UTF8_STRING"]
+  end
+
   config.call(conf, root)
 end
 
@@ -45,6 +52,7 @@ if ENV["CROSSCOMPILE_TARGET"] and !ENV["CROSSCOMPILE_TARGET"].empty?
 
     conf.cc do |cc|
       cc.command = ENV["CC"] || "#{prefix}-gcc"
+      cc.flags += ["-DMRB_UTF8_STRING"]
     end
 
     conf.linker do |linker|
