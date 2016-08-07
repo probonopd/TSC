e338f32292dbf2ab7d959740544f1289a3fec3d1
Quintus <quintus@quintilianus.eu>
2016-08-07 19:02:26 +0200

    Tell CMake which libraries the mruby external project builds.
    This is required for the "ninja" generator it appears. Cf. #559.

--- tsc/cmake/modules/ProvideMRuby.cmake
+++ tsc/cmake/modules/ProvideMRuby.cmake
@@ -4,6 +4,13 @@ message("-- Scripting engine enabled: building mruby statically")
 # for on the toplevel anyway.
 find_package(BISON REQUIRED)
 
+if(CMAKE_CROSSCOMPILING)
+  # Appearently no libmruby_core.a library when crosscompiling?
+  set(MRuby_LIBRARIES "${TSC_BINARY_DIR}/mruby/build/${HOST_TRIPLET}/lib/libmruby.a")
+else()
+  set(MRuby_LIBRARIES "${TSC_BINARY_DIR}/mruby/build/host/lib/libmruby.a" "${TSC_BINARY_DIR}/mruby/build/host/lib/libmruby_core.a")
+endif()
+
 # The CROSSCOMPILE_TARGET environment variable is used by our custom
 # mruby_tsc_build_config.rb build script for mruby. It is not part
 # of mruby's official build documentation.
@@ -14,13 +21,7 @@ ExternalProject_Add(
   CONFIGURE_COMMAND ""
   BUILD_IN_SOURCE 1
   BUILD_COMMAND ./minirake MRUBY_CONFIG=${TSC_SOURCE_DIR}/mruby_tsc_build_config.rb CROSSCOMPILE_TARGET=${HOST_TRIPLET}
+  BUILD_BYPRODUCTS ${MRuby_LIBRARIES}
   INSTALL_COMMAND "")
 
 set(MRuby_INCLUDE_DIR ${TSC_SOURCE_DIR}/../mruby/mruby/include)
-
-if(CMAKE_CROSSCOMPILING)
-  # Appearently no libmruby_core.a library when crosscompiling?
-  set(MRuby_LIBRARIES "${TSC_BINARY_DIR}/mruby/build/${HOST_TRIPLET}/lib/libmruby.a")
-else()
-  set(MRuby_LIBRARIES "${TSC_BINARY_DIR}/mruby/build/host/lib/libmruby.a" "${TSC_BINARY_DIR}/mruby/build/host/lib/libmruby_core.a")
-endif()
