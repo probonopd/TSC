746a448f9b10a85a300d35a29e94adf986924877
Quintus <quintus@quintilianus.eu>
2017-01-08 18:10:15 +0100

    Add Rect and Point structs.
    I hope to use these instead of the plain arrays in the future for
    anything that returns rectangles and points in the scripting API.

--- tsc/data/scripting/main.rb
+++ tsc/data/scripting/main.rb
@@ -8,3 +8,9 @@
 
 # Alias Armadillo to Army so that both names work.
 Army = Armadillo
+
+# A Point represents a X/Y position in a level.
+Point = Struct.new(:x, :y)
+
+# A Rect instance represents a rectangle.
+Rect = Struct.new(:x, :y, :width, :height)
