c5650ef53b9935be06c69f2d63e2052a075ebf78
Ryan Gonzalez <rymg19@gmail.com>
2017-07-10 17:10:39 -0500

    Get the latest SFML for building

--- travis.sh
+++ travis.sh
@@ -5,6 +5,10 @@ set -e
 mkdir install
 export INSTALL_PREFIX=$PWD/install
 
+curl -Lo sfml.tgz https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
+tar xvf sfml.tgz
+cp -r SFML-2.4.2/* $INSTALL_PREFIX
+
 hg clone -b v0-8 https://bitbucket.org/cegui/cegui
 cd cegui
 mkdir build
@@ -13,9 +17,9 @@ cmake -G Ninja .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
 ninja install
 cd ../..
 
-ls -R $INSTALL_PREFIX
-
 mkdir build
 cd build
 cmake -G Ninja ../tsc -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
 ninja install
+
+ls -R $INSTALL_PREFIX
